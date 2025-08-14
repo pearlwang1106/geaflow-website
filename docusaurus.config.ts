98965fb9ed18f6df2ef86102d38d8bf72b1f9a36
liukaiming-alipay <lkm01250564@antgroup.com>
2025-08-14 09:53:00 +0800

    feat: add deploy action

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -84,11 +84,6 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
@@ -122,7 +117,7 @@ const config: Config = {
         {
           type: 'docSidebar',
           sidebarId: 'communitySidebar',
-          position: 'right',
+          position: 'left',
           label: 'Community',
         },
         {
